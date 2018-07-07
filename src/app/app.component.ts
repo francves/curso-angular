3ff1adc98ea5c072f8721f9bf64cb0a89a03cfb8
Francisco Velásquez <francves1711@gmail.com>
2018-07-07 12:08:53 -0400

    directiva ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Primera app Fran!';
-  a = 3;
-  b = 4;
-  listo = true;
-  contador = 0;
-  nombre;
-  apellido;
+  lugares:any = [
+  {nombre: "Floristeria don pepe"},
+  {nombre: "Pizzería mamma mía!"},
+  {nombre: "Auto lavado carwash"}
+  ]
   constructor(){
-  	setTimeout(() => {
-  		this.listo = false;
-  		console.log("Hola");
-  	}, 4000)
-  }
-  hacerAlgo(){
-  	this.contador++;
+  
   }
 }
