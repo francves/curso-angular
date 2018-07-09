eb85dad5a50745281bbd272b018933de75e05f78
Francisco Velásquez <francves1711@gmail.com>
2018-07-08 22:39:16 -0400

    Parametros de ruta y parametros tipo query

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -9,12 +9,12 @@ export class LugaresComponent {
   lat:number = 10.0752569;
   lng:number = -69.3004637;
   lugares:any = [
-  {plan: 'pagado', cercania: 1, distancia: 1, active: false, nombre: "Floristeria don pepe"},
-  {plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: "Pizzería mamma mía!"},
-  {plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre: "Auto lavado carwash"},
-  {plan: 'gratuito', cercania: 3, distancia: 10, active: false, nombre: "Heladería icecream"},
-  {plan: 'pagado', cercania: 3, distancia: 35, active: true, nombre: "Zapatería el juanete"},
-  {plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: "Frutería my apple"}
+  {id: 1, plan: 'pagado', cercania: 1, distancia: 1, active: false, nombre: "Floristeria don pepe"},
+  {id: 2, plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: "Pizzería mamma mía!"},
+  {id: 3, plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre: "Auto lavado carwash"},
+  {id: 4, plan: 'gratuito', cercania: 3, distancia: 10, active: false, nombre: "Heladería icecream"},
+  {id: 5, plan: 'pagado', cercania: 3, distancia: 35, active: true, nombre: "Zapatería el juanete"},
+  {id: 6, plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: "Frutería my apple"}
   ]
   constructor(){
   
