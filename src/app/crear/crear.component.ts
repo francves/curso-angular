d3c10704da7a4a82ba0315cec1bdd89a34cd742f
Francisco Velásquez <francves1711@gmail.com>
2018-07-09 15:50:40 -0400

    Vista de edicion de lugares y agregando marcadores de los lugares a google map

--- src/app/crear/crear.component.ts
+++ src/app/crear/crear.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { LugaresService } from "../services/lugares.service";
 
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-crear',
@@ -9,18 +10,31 @@ import { LugaresService } from "../services/lugares.service";
 })
 export class CrearComponent {
 	lugar:any = {};
-	constructor(private lugaresService: LugaresService){
-
+	id:any = null;
+	constructor(private lugaresService: LugaresService, private route: ActivatedRoute){
+		this.id = this.route.snapshot.params['id'];
+		if(this.id != 'new'){
+			this.lugaresService.getLugar(this.id)
+				.valueChanges().subscribe((lugar) => {
+					this.lugar = lugar;
+				});
+		}
 	}
 	guardarLugar(){
-		var direccion = this.lugar.calle+','+this.lugar.ciudad+','+this.lugar.pais;
+		var direccion = this.lugar.calle+','+this.lugar.ciudad+','+this.lugar.estado+','+this.lugar.pais;
 		this.lugaresService.obtenerGeoData(direccion)
 			.subscribe((result) => {
 				this.lugar.lat = result.json().results[0].geometry.location.lat;
 				this.lugar.lng = result.json().results[0].geometry.location.lng;
-				this.lugar.id = Date.now();
-				this.lugaresService.guardarLugar(this.lugar);
-				alert("Negocio guardado con éxito");
+				
+				if(this.id != 'new'){
+					this.lugaresService.editarLugar(this.lugar);
+					alert("Negocio editado con éxito");
+				}else{
+					this.lugar.id = Date.now();
+					this.lugaresService.guardarLugar(this.lugar);
+					alert("Negocio guardado con éxito");
+				}
 				this.lugar = {};
 			});
 	}
